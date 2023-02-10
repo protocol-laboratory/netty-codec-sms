87cca3eb9d21d10e265618d6b7c0e896e6c03409
Lico-Tom <95178212+Lico-Tom@users.noreply.github.com>
2023-02-10 12:48:46 +0800

    sgip (bind bindResp) encode and decode. (#5)

--- netty-codec-sgip/src/main/java/io/github/protocol/codec/sgip/SgipDecoder.java
+++ netty-codec-sgip/src/main/java/io/github/protocol/codec/sgip/SgipDecoder.java
@@ -1,6 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.github.protocol.codec.sgip;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+import java.nio.charset.StandardCharsets;
 
 public class SgipDecoder extends LengthFieldBasedFrameDecoder {
 
@@ -11,4 +34,63 @@ public class SgipDecoder extends LengthFieldBasedFrameDecoder {
     public SgipDecoder(int maxBytesInMessage) {
         super(maxBytesInMessage, 0, 4, -4, 0);
     }
+
+    @Override
+    protected SgipMessage decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+        ByteBuf frame = (ByteBuf) super.decode(ctx, in);
+        if (frame == null) {
+            return null;
+        }
+        try {
+            return decode(frame);
+        } finally {
+            frame.release();
+        }
+    }
+
+    SgipMessage decode(ByteBuf frame) {
+        int messageLength = frame.readInt();
+        int commandId = frame.readInt();
+        long sequenceNumber = frame.readLong();
+        SgipHeader sgipHeader = new SgipHeader(messageLength, commandId, sequenceNumber);
+        switch (commandId) {
+            case SgipConst.BIND_ID:
+                return new SgipBind(sgipHeader, decodeBind(frame));
+            case SgipConst.BIND_RESP_ID:
+                return new SgipBindResp(sgipHeader, decodeBindResp(frame));
+            default:
+                throw new IllegalArgumentException("Unknown commandId: " + commandId);
+        }
+    }
+
+    private SgipBindBody decodeBind(ByteBuf frame) {
+        byte loginType = frame.readByte();
+        String loginName = readString(frame, SgipConst.LEN_LOGIN_NAME);
+        String loginPassword = readString(frame, SgipConst.LEN_LOGIN_PASSWORD);
+        String reserve = readString(frame, SgipConst.LEN_BIND_RESERVE);
+        return new SgipBindBody(loginType, loginName, loginPassword, reserve);
+    }
+
+    private SgipBindRespBody decodeBindResp(ByteBuf frame) {
+        byte result = frame.readByte();
+        String reserve = readString(frame, SgipConst.LEN_BIND_RESP_RESERVE);
+        return new SgipBindRespBody(result, reserve);
+    }
+
+    private String readString(ByteBuf byteBuf, int length) {
+        int idx = 0;
+        byte[] bytes = new byte[length];
+        for (int i = 0; i < length; i++) {
+            byte b = byteBuf.readByte();
+            if (b == 0 && idx == 0) {
+                idx = i;
+            }
+            bytes[i] = b;
+        }
+        if (idx == 0) {
+            idx = length;
+        }
+        return new String(bytes, 0, idx, StandardCharsets.UTF_8);
+    }
+
 }
--- netty-codec-sgip/src/main/java/io/github/protocol/codec/sgip/SgipEncoder.java
+++ netty-codec-sgip/src/main/java/io/github/protocol/codec/sgip/SgipEncoder.java
@@ -19,10 +19,12 @@
 
 package io.github.protocol.codec.sgip;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @ChannelHandler.Sharable
@@ -33,5 +35,54 @@ public class SgipEncoder extends MessageToMessageEncoder<SgipMessage> {
     @Override
     protected void encode(ChannelHandlerContext channelHandlerContext, SgipMessage sgipMessage,
                           List<Object> list) throws Exception {
+        list.add(doEncode(channelHandlerContext, sgipMessage));
+    }
+
+    ByteBuf doEncode(ChannelHandlerContext ctx, SgipMessage message) {
+        switch (message.header().commandId()) {
+            case SgipConst.BIND_ID:
+                return encodeBind(ctx, (SgipBind) message);
+            case SgipConst.BIND_RESP_ID:
+                return encodeBindResp(ctx, (SgipBindResp) message);
+            default:
+                throw new IllegalArgumentException("Unknown command id: " + message.header().commandId());
+        }
+    }
+
+    private ByteBuf encodeBind(ChannelHandlerContext ctx, SgipBind message) {
+        ByteBuf buf = ctx.alloc().buffer(SgipConst.LEN_BIND_MSG);
+        writeHeader(buf, message.header(), SgipConst.LEN_BIND_MSG);
+        buf.writeByte(message.body().loginType());
+        writeString(buf, message.body().loginName(), SgipConst.LEN_LOGIN_NAME);
+        writeString(buf, message.body().loginPassword(), SgipConst.LEN_LOGIN_PASSWORD);
+        writeString(buf, message.body().reserve(), SgipConst.LEN_BIND_RESERVE);
+        return buf;
+    }
+
+    private ByteBuf encodeBindResp(ChannelHandlerContext ctx, SgipBindResp message) {
+        ByteBuf buf = ctx.alloc().buffer(SgipConst.LEN_BIND_RESP_MSG);
+        writeHeader(buf, message.header(), SgipConst.LEN_BIND_MSG);
+        buf.writeByte(message.body().result());
+        writeString(buf, message.body().reserve(), SgipConst.LEN_BIND_RESP_RESERVE);
+        return buf;
+    }
+
+    private void writeString(ByteBuf buf, String str, int length) {
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
+        int len = Math.min(bytes.length, length);
+        buf.writeBytes(bytes, 0, len);
+        for (int i = len; i < length; i++) {
+            buf.writeByte(0);
+        }
+    }
+
+    private void writeHeader(ByteBuf buf, SgipHeader header, int size) {
+        if (header.messageLength() == 0) {
+            buf.writeInt(size);
+        } else {
+            buf.writeInt(header.messageLength());
+        }
+        buf.writeInt(header.commandId());
+        buf.writeLong(header.sequenceNumber());
     }
 }
