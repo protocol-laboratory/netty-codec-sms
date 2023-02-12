235b4fc572713b053fe918c74aec74415e429b0d
ZhangJian He <shoothzj@gmail.com>
2023-02-12 10:14:08 +0800

    fix style

--- netty-codec-cngp/src/main/java/io/github/protocol/codec/cngp/CngpConst.java
+++ netty-codec-cngp/src/main/java/io/github/protocol/codec/cngp/CngpConst.java
@@ -44,14 +44,21 @@ public class CngpConst {
     public static final int EXIT_RESP_ID = 0x80000006;
 
     public static final int LEN_AT_TIME = 17;
+
     public static final int LEN_CHARGE_TERM_ID = 21;
+
     public static final int LEN_CLIENT_ID = 10;
+
     public static final int LEN_DELIVER_MSG =  CngpConst.LEN_HEADER + 69;
 
     public static final int LEN_DELIVER_DEST_TERM_ID = 21;
+
     public static final int LEN_DELIVER_MESSAGE_ID = 10;
+
     public static final int LEN_DELIVER_RESP_MSG = CngpConst.LEN_HEADER +  10;
+
     public static final int LEN_DELIVER_SRC_TERM_ID = 21;
+
     public static final int LEN_DEST_TERM_ID = 21;
 
     public static final int LEN_FEE_CODE = 6;
@@ -61,21 +68,31 @@ public class CngpConst {
     public static final int LEN_FEE_USER_TYPE = 1;
 
     public static final int LEN_HEADER = 16;
+
     public static final int LEN_LOGIN_AUTHENTICATOR_CLIENT = 16;
 
     public static final int LEN_LOGIN_RESP_AUTHENTICATOR_CLIENT = 16;
+
     public static final int LEN_LOGIN_MSG = CngpConst.LEN_HEADER + 32;
+
     public static final int LEN_LOGIN_RESP_MSG = CngpConst.LEN_HEADER + 17;
+
     public static final int LEN_MESSAGE_FORMAT = 1;
+
+    public static final int LEN_MESSAGE_ID = 10;
+
     public static final int LEN_RECV_TIME = 14;
+
     public static final int LEN_SERVICE_ID = 10;
+
     public static final int LEN_SP_ID = 10;
+
     public static final int LEN_SUBMIT_RESP_MSG = CngpConst.LEN_HEADER + 10;
+
     public static final int LEN_SUBMIT_MSG = CngpConst.LEN_HEADER + 111;
 
     public static final int LEN_SUBMIT_SRC_TERM_ID = 21;
 
-    public static final int LEN_MESSAGE_ID = 10;
     public static final int LEN_VALID_TIME = 17;
 
 }
