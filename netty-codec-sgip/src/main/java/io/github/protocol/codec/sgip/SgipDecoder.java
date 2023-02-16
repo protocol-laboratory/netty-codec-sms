9574d44418ab6f4db3f73f2b88125301a7b3714e
Lico-Tom <95178212+Lico-Tom@users.noreply.github.com>
2023-02-16 19:24:24 +0800

    sgip encode and decode. (#10)

--- netty-codec-sgip/src/main/java/io/github/protocol/codec/sgip/SgipDecoder.java
+++ netty-codec-sgip/src/main/java/io/github/protocol/codec/sgip/SgipDecoder.java
@@ -58,6 +58,30 @@ public class SgipDecoder extends LengthFieldBasedFrameDecoder {
                 return new SgipBind(sgipHeader, decodeBind(frame));
             case SgipConst.BIND_RESP_ID:
                 return new SgipBindResp(sgipHeader, decodeBindResp(frame));
+            case SgipConst.UNBIND_ID:
+                return new SgipUnbind(sgipHeader);
+            case SgipConst.UNBIND_RESP_ID:
+                return new SgipUnbindResp(sgipHeader);
+            case SgipConst.SUBMIT_ID:
+                return new SgipSubmit(sgipHeader, decodeSubmit(frame));
+            case SgipConst.SUBMIT_RESP_ID:
+                return new SgipSubmitResp(sgipHeader, decodeSubmitResp(frame));
+            case SgipConst.DELIVER_ID:
+                return new SgipDeliver(sgipHeader, decodeDeliver(frame));
+            case SgipConst.DELIVER_RESP_ID:
+                return new SgipDeliverResp(sgipHeader, decodeDeliverResp(frame));
+            case SgipConst.REPORT_ID:
+                return new SgipReport(sgipHeader, decodeReport(frame));
+            case SgipConst.REPORT_RESP_ID:
+                return new SgipReportResp(sgipHeader, decodeReportResp(frame));
+            case SgipConst.USERRPT_ID:
+                return new SgipUserRpt(sgipHeader, decodeUserRpt(frame));
+            case SgipConst.USERRPT_RESP_ID:
+                return new SgipUserRptResp(sgipHeader, decodeUserRptResp(frame));
+            case SgipConst.TRACE_ID:
+                return new SgipTrace(sgipHeader, decodeTrace(frame));
+            case SgipConst.TRACE_RESP_ID:
+                return new SgipTraceResp(sgipHeader, decodeTraceResp(frame));
             default:
                 throw new IllegalArgumentException("Unknown commandId: " + commandId);
         }
@@ -75,6 +99,98 @@ public class SgipDecoder extends LengthFieldBasedFrameDecoder {
         byte result = frame.readByte();
         String reserve = readString(frame, SgipConst.LEN_BIND_RESP_RESERVE);
         return new SgipBindRespBody(result, reserve);
+    }
+
+    private SgipSubmitBody decodeSubmit(ByteBuf frame) {
+        String spNumber = readCString(frame);
+        String chargeNumber = readCString(frame);
+        String userNumber = readCString(frame);
+        String corpId = readCString(frame);
+        String serviceType = readCString(frame);
+        String feeType = readCString(frame);
+        String feeValue = readCString(frame);
+        String givenValue = readCString(frame);
+        String agentFlag = readCString(frame);
+        String morelatetoMTFlag = readCString(frame);
+        long priority = frame.readLong();
+        String expireTime = readCString(frame);
+        String scheduleTime = readCString(frame);
+        long reportFlag = frame.readLong();
+        long messageType = frame.readLong();
+        long tpPid = frame.readLong();
+        long tpUdhi = frame.readLong();
+        long messageCoding = frame.readLong();
+        long messageContent = frame.readLong();
+        return new SgipSubmitBody(spNumber, chargeNumber, userNumber, corpId, serviceType, feeType, feeValue,
+                givenValue, agentFlag, morelatetoMTFlag, priority, expireTime, scheduleTime, reportFlag,
+                messageType, tpPid, tpUdhi, messageCoding, messageContent);
+    }
+
+    private SgipSubmitRespBody decodeSubmitResp(ByteBuf frame) {
+        return new SgipSubmitRespBody(frame.readLong());
+    }
+
+    private SgipDeliverBody decodeDeliver(ByteBuf frame) {
+        String userNumber = readCString(frame);
+        String spNumber = readCString(frame);
+        String tpPid = readCString(frame);
+        String tpUdhi = readCString(frame);
+        long messageCoding = frame.readLong();
+        long messageContent = frame.readLong();
+        return new SgipDeliverBody(userNumber, spNumber, tpPid, tpUdhi, messageCoding, messageContent);
+    }
+
+    private SgipDeliverRespBody decodeDeliverResp(ByteBuf frame) {
+        return new SgipDeliverRespBody(frame.readLong());
+    }
+
+    private SgipReportBody decodeReport(ByteBuf frame) {
+        long submitSequenceNumber = frame.readLong();
+        long reportType = frame.readLong();
+        String userNumber = readCString(frame);
+        long state = frame.readLong();
+        long errorCode = frame.readLong();
+        return new SgipReportBody(submitSequenceNumber, reportType, userNumber, state, errorCode);
+    }
+
+    private SgipReportRespBody decodeReportResp(ByteBuf frame) {
+        return new SgipReportRespBody(frame.readLong());
+    }
+
+    private SgipUserRptBody decodeUserRpt(ByteBuf frame) {
+        String spNumber = readCString(frame);
+        String userNumber = readCString(frame);
+        long userCondition = frame.readLong();
+        return new SgipUserRptBody(spNumber, userNumber, userCondition);
+    }
+
+    private SgipUserRptRespBody decodeUserRptResp(ByteBuf frame) {
+        return new SgipUserRptRespBody(frame.readLong());
+    }
+
+    private SgipTraceBody decodeTrace(ByteBuf frame) {
+        long submitSequenceNumber = frame.readLong();
+        String userNumber = readCString(frame);
+        return new SgipTraceBody(submitSequenceNumber, userNumber);
+    }
+
+    private SgipTraceRespBody decodeTraceResp(ByteBuf frame) {
+        long count = frame.readLong();
+        long result = frame.readLong();
+        String nodeId = readCString(frame);
+        String receiveTime = readCString(frame);
+        String sendTime = readCString(frame);
+        return new SgipTraceRespBody(count, result, nodeId, receiveTime, sendTime);
+    }
+
+    private String readCString(ByteBuf frame) {
+        int length = frame.bytesBefore((byte) 0);
+        if (length == -1) {
+            throw new IllegalArgumentException("No \\0 found");
+        }
+        CharSequence charSequence = frame.readCharSequence(length, StandardCharsets.UTF_8);
+        frame.skipBytes(1);
+        return charSequence.toString();
     }
 
     private String readString(ByteBuf byteBuf, int length) {
--- netty-codec-sgip/src/main/java/io/github/protocol/codec/sgip/SgipEncoder.java
+++ netty-codec-sgip/src/main/java/io/github/protocol/codec/sgip/SgipEncoder.java
@@ -44,6 +44,29 @@ public class SgipEncoder extends MessageToMessageEncoder<SgipMessage> {
                 return encodeBind(ctx, (SgipBind) message);
             case SgipConst.BIND_RESP_ID:
                 return encodeBindResp(ctx, (SgipBindResp) message);
+            case SgipConst.UNBIND_ID:
+            case SgipConst.UNBIND_RESP_ID:
+                return encodeHeader(ctx, message.header());
+            case SgipConst.SUBMIT_ID:
+                return encodeSubmit(ctx, (SgipSubmit) message);
+            case SgipConst.SUBMIT_RESP_ID:
+                return encodeSubmitResp(ctx, (SgipSubmitResp) message);
+            case SgipConst.DELIVER_ID:
+                return encodeDeliver(ctx, (SgipDeliver) message);
+            case SgipConst.DELIVER_RESP_ID:
+                return encodeDeliverResp(ctx, (SgipDeliverResp) message);
+            case SgipConst.REPORT_ID:
+                return encodeReport(ctx, (SgipReport) message);
+            case SgipConst.REPORT_RESP_ID:
+                return encodeReportResp(ctx, (SgipReportResp) message);
+            case SgipConst.USERRPT_ID:
+                return encodeUserRpt(ctx, (SgipUserRpt) message);
+            case SgipConst.USERRPT_RESP_ID:
+                return encodeUserRptResp(ctx, (SgipUserRptResp) message);
+            case SgipConst.TRACE_ID:
+                return encodeTrace(ctx, (SgipTrace) message);
+            case SgipConst.TRACE_RESP_ID:
+                return encodeTraceResp(ctx, (SgipTraceResp) message);
             default:
                 throw new IllegalArgumentException("Unknown command id: " + message.header().commandId());
         }
@@ -65,6 +88,173 @@ public class SgipEncoder extends MessageToMessageEncoder<SgipMessage> {
         buf.writeByte(message.body().result());
         writeString(buf, message.body().reserve(), SgipConst.LEN_BIND_RESP_RESERVE);
         return buf;
+    }
+
+
+    private ByteBuf encodeSubmit(ChannelHandlerContext ctx, SgipSubmit message) {
+        int bodyLen = submitBodyLen(message.body());
+        ByteBuf buf = ctx.alloc().buffer(bodyLen);
+        writeHeader(buf, message.header(), bodyLen);
+        SgipSubmitBody submitBody = message.body();
+        writeString(buf, submitBody.spNumber(), len(submitBody.spNumber()));
+        writeString(buf, submitBody.chargeNumber(), len(submitBody.chargeNumber()));
+        writeString(buf, submitBody.userNumber(), len(submitBody.userNumber()));
+        writeString(buf, submitBody.corpId(), len(submitBody.corpId()));
+        writeString(buf, submitBody.serviceType(), len(submitBody.serviceType()));
+        writeString(buf, submitBody.feeType(), len(submitBody.feeType()));
+        writeString(buf, submitBody.feeValue(), len(submitBody.feeValue()));
+        writeString(buf, submitBody.givenValue(), len(submitBody.givenValue()));
+        writeString(buf, submitBody.agentFlag(), len(submitBody.agentFlag()));
+        writeString(buf, submitBody.morelatetoMTFlag(), len(submitBody.morelatetoMTFlag()));
+        buf.writeLong(submitBody.priority());
+        writeString(buf, submitBody.expireTime(), len(submitBody.expireTime()));
+        writeString(buf, submitBody.scheduleTime(), len(submitBody.scheduleTime()));
+        buf.writeLong(submitBody.reportFlag());
+        buf.writeLong(submitBody.messageType());
+        buf.writeLong(submitBody.tpPid());
+        buf.writeLong(submitBody.tpUdhi());
+        buf.writeLong(submitBody.messageCoding());
+        buf.writeLong(submitBody.messageContent());
+        return buf;
+    }
+
+
+    private ByteBuf encodeSubmitResp(ChannelHandlerContext ctx, SgipSubmitResp message) {
+        SgipSubmitRespBody body = message.body();
+        ByteBuf buf = ctx.alloc().buffer(SgipConst.LEN_SUBMIT_RESP_MSG);
+        writeHeader(buf, message.header(), SgipConst.LEN_SUBMIT_RESP_MSG);
+        buf.writeLong(body.result());
+        return buf;
+    }
+
+
+    private ByteBuf encodeDeliver(ChannelHandlerContext ctx, SgipDeliver message) {
+        SgipDeliverBody deliverBody = message.body();
+        int bodyLen = deliverBodyLen(deliverBody);
+        ByteBuf buf = ctx.alloc().buffer(bodyLen);
+        writeHeader(buf, message.header(), bodyLen);
+        writeString(buf, deliverBody.userNumber(), len(deliverBody.userNumber()));
+        writeString(buf, deliverBody.spNumber(), len(deliverBody.spNumber()));
+        writeString(buf, deliverBody.tpPid(), len(deliverBody.tpPid()));
+        writeString(buf, deliverBody.tpUdhi(), len(deliverBody.tpUdhi()));
+        buf.writeLong(deliverBody.messageCoding());
+        buf.writeLong(deliverBody.messageContent());
+        return buf;
+    }
+
+    private ByteBuf encodeDeliverResp(ChannelHandlerContext ctx, SgipDeliverResp message) {
+        ByteBuf buf = ctx.alloc().buffer(SgipConst.LEN_DELIVER_RESP_MSG);
+        writeHeader(buf, message.header(), SgipConst.LEN_DELIVER_RESP_MSG);
+        buf.writeLong(message.body().result());
+        return buf;
+    }
+
+    private ByteBuf encodeReport(ChannelHandlerContext ctx, SgipReport message) {
+        SgipReportBody reportBody = message.body();
+        int bodyLen = reportBodyLen(reportBody);
+        ByteBuf buf = ctx.alloc().buffer(bodyLen);
+        writeHeader(buf, message.header(), bodyLen);
+        buf.writeLong(reportBody.submitSequenceNumber());
+        buf.writeLong(reportBody.reportType());
+        writeString(buf, reportBody.userNumber(), len(reportBody.userNumber()));
+        buf.writeLong(reportBody.state());
+        buf.writeLong(reportBody.errorCode());
+        return buf;
+    }
+
+    private ByteBuf encodeReportResp(ChannelHandlerContext ctx, SgipReportResp message) {
+        SgipReportRespBody reportRespBody = message.body();
+        ByteBuf buf = ctx.alloc().buffer(SgipConst.LEN_REPORT_RESP_MSG);
+        writeHeader(buf, message.header(), SgipConst.LEN_REPORT_RESP_MSG);
+        buf.writeLong(reportRespBody.result());
+        return buf;
+    }
+
+
+    private ByteBuf encodeUserRpt(ChannelHandlerContext ctx, SgipUserRpt message) {
+        SgipUserRptBody userRptBody = message.body();
+        int bodyLen = userRptBodyLen(userRptBody);
+        ByteBuf buf = ctx.alloc().buffer(bodyLen);
+        writeHeader(buf, message.header(), bodyLen);
+        writeString(buf, userRptBody.spNumber(), len(userRptBody.spNumber()));
+        writeString(buf, userRptBody.userNumber(), len(userRptBody.userNumber()));
+        buf.writeLong(userRptBody.userCondition());
+        return buf;
+    }
+    private ByteBuf encodeUserRptResp(ChannelHandlerContext ctx, SgipUserRptResp message) {
+        ByteBuf buf = ctx.alloc().buffer(SgipConst.LEN_USERRPT_RESP_MSG);
+        writeHeader(buf, message.header(), SgipConst.LEN_USERRPT_RESP_MSG);
+        buf.writeLong(message.body().result());
+        return buf;
+    }
+
+    private ByteBuf encodeTrace(ChannelHandlerContext ctx, SgipTrace message) {
+        SgipTraceBody traceBody = message.body();
+        int bodyLen = traceBodyLen(traceBody);
+        ByteBuf buf = ctx.alloc().buffer(bodyLen);
+        writeHeader(buf, message.header(), bodyLen);
+        buf.writeLong(message.body().submitSequenceNumber());
+        writeString(buf, traceBody.userNumber(), len(traceBody.userNumber()));
+        return buf;
+    }
+
+    private ByteBuf encodeTraceResp(ChannelHandlerContext ctx, SgipTraceResp message) {
+        SgipTraceRespBody traceRespBody = message.body();
+        int bodyLen = traceRespBodyLen(traceRespBody);
+        ByteBuf buf = ctx.alloc().buffer(bodyLen);
+        writeHeader(buf, message.header(), bodyLen);
+        buf.writeLong(message.body().count());
+        buf.writeLong(message.body().result());
+        writeString(buf, traceRespBody.nodeId(), len(traceRespBody.nodeId()));
+        writeString(buf, traceRespBody.receiveTime(), len(traceRespBody.receiveTime()));
+        writeString(buf, traceRespBody.sendTime(), len(traceRespBody.sendTime()));
+        return buf;
+    }
+
+    private ByteBuf encodeHeader(ChannelHandlerContext ctx, SgipHeader header) {
+        ByteBuf buf = ctx.alloc().buffer(SgipConst.LEN_HEADER);
+        buf.writeInt(header.messageLength());
+        buf.writeInt(header.commandId());
+        buf.writeLong(header.sequenceNumber());
+        return buf;
+    }
+
+    private int deliverBodyLen(SgipDeliverBody body) {
+        return len(body.userNumber())
+                + len(body.spNumber())
+                + len(body.tpPid())
+                + len(body.tpUdhi())
+                + SgipConst.LEN_DELIVER_MSG;
+    }
+
+    private int submitBodyLen(SgipSubmitBody body) {
+        return len(body.spNumber()) + len(body.chargeNumber()) + len(body.userNumber()) + len(body.corpId())
+                + len(body.serviceType()) + len(body.feeType()) + len(body.feeValue()) + len(body.givenValue())
+                + len(body.givenValue()) + len(body.morelatetoMTFlag()) + len(body.expireTime())
+                + len(body.scheduleTime()) + SgipConst.LEN_SUBMIT_MSG;
+    }
+
+    private int reportBodyLen(SgipReportBody body) {
+        return len(body.userNumber()) + SgipConst.LEN_REPORT_MSG;
+    }
+
+    private int userRptBodyLen(SgipUserRptBody body) {
+        return len(body.spNumber()) + len(body.userNumber()) + SgipConst.LEN_USERRPT_MSG;
+    }
+
+    private int traceBodyLen(SgipTraceBody body) {
+        return len(body.userNumber()) + SgipConst.LEN_TRACE_MSG;
+    }
+
+    private int traceRespBodyLen(SgipTraceRespBody body) {
+        return len(body.nodeId())
+                + len(body.receiveTime())
+                + len(body.sendTime())
+                + SgipConst.LEN_TRACE_RESP_MSG;
+    }
+
+    private int len(String str) {
+        return str == null ? 1 : str.length() + 1;
     }
 
     private void writeString(ByteBuf buf, String str, int length) {
